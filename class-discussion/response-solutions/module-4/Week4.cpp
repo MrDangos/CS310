7756c6367032884f88ed5574b9fda9b3095758ab
Hugo Ramirez <hrr4114715@gmail.com>
2026-04-09 08:37:58 -0500

    updated file name to week4

---
+++ class-discussion/response-solutions/module-4/Week4.cpp
@@ -0,0 +1,34 @@
+/*
+ *Student Name: Daniel Preller
+ *File Name: Week4.cpp
+ *Date: 4/6/2026
+ */
+
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    srand(time0);
+    /*srand() function seeded with the time is used for simplicity, but it should be avoided in real code because it is
+    highly deterministic, very insecure, and not evenly distributed*/
+
+    // Generates two random-ish numbers from 1 to 100 and multiplies them together to find the solution
+    int number1 = rand() % 100 + 1;
+    int number2 = rand() % 100 + 1;
+    int solution = number1 * number2;
+
+    int answer = 0;//Initializes answer to an invalid number (possible solutions to generate range from 1 to 10,000)
+
+    while answer == solution
+        cout << "What is " << number1 << " * " << number2 << "? ";
+        cin >> answer;
+
+        if (answer == solution) {
+            cout << "Correct! Press 'enter' to exit." << endl;
+        } else {
+            cout << "Incorrect. ";// endl intentionally omitted to keep the question on the same line
+        }
+    getch();//Halts and waits for user input
+}
