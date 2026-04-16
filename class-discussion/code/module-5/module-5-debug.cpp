db09732fa9b4cc95f2e71b23c876046c8f656c65
Hugo Ramirez <hrr4114715@gmail.com>
2026-04-15 20:21:24 -0500

    adding module 5 debug code

---
+++ class-discussion/code/module-5/module-5-debug.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int addNumbers(int a, double b)
+
+    return a + b;
+}
+
+int main() {
+    int x = 5;
+    int y = 10
+    int result = addNumbers(x, y);
+
+    cout << "The sum is: " << result << endl;
+
+    return 0;
+}
